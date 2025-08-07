d9e5781faef682862514b6fccf9cce516b0e325a
Abdulmatin Adeniji <abdulmatinadeniji@gmail.com>
2025-08-07 22:39:26 +0100

    changing domain in cors

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -3,21 +3,21 @@ import cors from 'cors';
 
 // Define the allowed origin. For development, this is your Next.js frontend URL.
 // For production, you would change this to 'https://www.tfbstudios.com'
-const allowedOrigins = ['http://localhost:3000', 'https://frontend.tfbstudios.com', "https://tfb-studios-frontend.up.railway.app"];
+const allowedOrigins = ['http://localhost:3000', "https://tfb-studios-frontend.up.railway.app"];
 
 const corsOptions = {
   // The origin property must be set to your specific frontend URL. A wildcard ('*') will not work for credentials.
   origin: (origin, callback) => {
     // allow requests with no origin (like mobile apps or curl requests)
-    // if (!origin) return callback(null, true);
-    // if (allowedOrigins.indexOf(origin) === -1) {
-    //   const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-    //   return callback(new Error(msg), false);
-    // }
-    // return callback(null, true);
     if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error('CORS blocked this origin'), false);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+    // if (!origin) return callback(null, true);
+    // if (allowedOrigins.includes(origin)) return callback(null, true);
+    // callback(new Error('CORS blocked this origin'), false);
   },
   credentials: true,
   exposedHeaders: ['Set-Cookie'],
